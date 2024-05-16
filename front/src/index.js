dbef3d92bef2a3bb2f7fa0e2d4ff1207754195aa
IlyaBen <profipingvin@gmail.com>
2024-05-16 13:55:40 +0700

    Update deploy

--- front/src/index.js
+++ front/src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL
+const currentURL = document.URL
+
+if (currentURL.includes("176.109.99.197") || currentURL.includes("enchik-pet")) {
+    axios.defaults.baseURL = "http://176.109.99.197:8008/"
+} else {
+    axios.defaults.baseURL = "http://localhost:8008/"
+}
+
 axios.defaults.withCredentials = true
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
